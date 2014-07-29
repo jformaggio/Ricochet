17e9e25109c3099c15e067e20ee5ed0860416daa
Joseph Formaggio <josephf@mit.edu>
2014-07-29 14:53:15 -0400

    Starting from morpho...

---
+++ sensitivity/run_krypton_analysis.py
@@ -0,0 +1,38 @@
+import pystanLoad as pyL
+
+casheDirectory = './cache'
+
+# Below is an example of running the analysis on experimental data.
+
+runType = "Data"
+
+if runType=="Data":
+    theModel = './krypton_analysis.stan'
+    theDataFiles = {'header':'./data/krypton_run.header.data', 'data':'./data/krypton_run.data'}
+    theSample= "./results/test_analysis.out"
+    thePlots = ['TotalField','TrappingField','MainField','frequency','KE','stheta']
+else :
+    theModel = './krypton_generator.stan'
+    theDataFiles = {'header':'./data/krypton_run.header.data', 'mc':'./data/krypton_monte_carlo.data'}
+    theSample= "./results/test_generator.out"
+    thePlots = ['TotalField','TrappingField','MainField','KE','frequency','freq_data','dfdt']
+
+# Run fitter
+
+theData = pyL.stan_data_files(theDataFiles)
+
+fit = pyL.stan_cache(model_code= theModel, 
+		 cashe_dir= casheDirectory,
+		 data=theData,
+		 algorithm = 'NUTS', 
+		 iter=2500, chains=16,
+		 sample_file=theSample)
+
+# Print results and make plots
+
+print(fit)
+
+for key in thePlots:
+    fit.plot(key)
+
+pyL.plt.show()
